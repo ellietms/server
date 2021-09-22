c5a6bba192e1d8c35755b1932c7e4f54ffe44275
ellie <ellie.tms1371@gmail.com>
2021-09-22 17:32:04 +0100

    setup: added routes file to server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-let express = require('express');
-let app = express();
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const routes = require('./routes')
+const PORT = process.env.PORT;
+
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routes)
 
 
 
 app.listen(5000, () => {
-    console.log(" Server is running ")
+    console.log(`Server is listening to port ${PORT}`)
 })
+
+
+module.exports = app;
